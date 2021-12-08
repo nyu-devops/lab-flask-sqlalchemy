e305bdfd09ecf11b5526114c4a7d429ec4c7c846
John Rofrano <johnnyroy@johnrofrano.com>
2021-12-08 07:33:55 -0500

    updated protocol to the new 'postgresql'

--- tests/test_server.py
+++ tests/test_server.py
@@ -28,7 +28,7 @@ from app.models import Pet, Category
 from app import server, db
 
 #DATABASE_URI = 'mysql+pymysql://root:passw0rd@localhost:3306/test'
-DATABASE_URI = os.getenv('DATABASE_URI', "postgres://postgres:postgres@localhost:5432/postgres")
+DATABASE_URI = os.getenv('DATABASE_URI', "postgresql://postgres:postgres@localhost:5432/postgres")
 
 ######################################################################
 #  T E S T   C A S E S
@@ -340,7 +340,6 @@ class TestPetServer(unittest.TestCase):
         """ Truncates all of the tables """
         meta = db.metadata
         for table in reversed(meta.sorted_tables):
-            print 'Clear table %s' % table
             session.execute(table.delete())
         session.commit()
 
